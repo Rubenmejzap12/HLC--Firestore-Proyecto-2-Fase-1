581ced55f53a4cbcbf16d5c65ca3a4d742a35611
Mejías Zapata, Rubén <94845866+Rubenmejzap12@users.noreply.github.com>
2023-01-18 12:53:03 +0100

    fase 2 (Pagina 2 creada)

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,10 +2,14 @@ import { Component } from '@angular/core';
 import { Aeropuerto } from '../tarea';
 
 import { FirestoreService } from '../firestore.service';
+import {Router} from '@angular/router';
+
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  
 })
 export class HomePage {
 
@@ -14,10 +18,9 @@ export class HomePage {
     id: "",
     data: {} as Aeropuerto
   }];
-  constructor(private firestoreService: FirestoreService) {
+  constructor(private firestoreService: FirestoreService, private router: Router) {
     // Crear una tarea vacia al empezar
     this.aeropuertoEditando = {} as Aeropuerto;
-
     this.obtenerListaAeropuertos();
   }
 
@@ -42,5 +45,33 @@ export class HomePage {
       })
     }
     )
+  }
+  idAeropuertoSelec: string;
+
+  selecAeropuerto(aeropuertoSelec) {
+    console.log("Aeropuerto seleccionada: ");
+    console.log(aeropuertoSelec);
+    this.idAeropuertoSelec = aeropuertoSelec.id;
+    this.aeropuertoEditando.nombre = aeropuertoSelec.data.nombre;
+    this.aeropuertoEditando.continente = aeropuertoSelec.data.continente;
+    this.router.navigate(['/detalle', this.idAeropuertoSelec]);
+  }
+
+  clicBotonBorrar() {
+    this.firestoreService.borrar("aeropuertos", this.idAeropuertoSelec).then(() => {
+      // Actualizar la lista completa
+      this.obtenerListaAeropuertos();
+      // Limpiar datos de pantalla
+      this.aeropuertoEditando = {} as Aeropuerto;
+    })
+  }
+
+  clicBotonModificar() {
+    this.firestoreService.actualizar("aeropuertos", this.idAeropuertoSelec, this.aeropuertoEditando).then(() => {
+      // Actualizar la lista completa
+      this.obtenerListaAeropuertos();
+      // Limpiar datos de pantalla
+      this.aeropuertoEditando = {} as Aeropuerto;
+    })
   }
 }
